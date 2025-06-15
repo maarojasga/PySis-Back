b0e25c1e27ff3ad46a1124bd8ed7aebab6beabd9
Arx <maarojasga@unal.edu.co>
2025-06-14 22:37:16 -0500

    Update database

--- statistics_service/app/database.py
+++ statistics_service/app/database.py
@@ -5,9 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////data/pysis.db")
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
